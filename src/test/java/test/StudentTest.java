0d51ea913be9cfc0e89a52a6ebe4af892a4ec55d
imayer95 <ingridmayer88@gmail.com>
2017-05-23 13:36:29 +0300

    ingriiid test picat

--- src/test/java/test/StudentTest.java
+++ src/test/java/test/StudentTest.java
@@ -34,7 +34,7 @@ public class StudentTest {
     @Test
     public void setRegNumber() throws Exception {
         s1.setRegNumber("studentul1");
-        assertEquals("setRegNumber crapat", "studentul1",s1.getRegNumber());
+        assertEquals("setRegNumber crapat", "studentul3",s1.getRegNumber());
 
         s1.setRegNumber("studentul2");
         assertEquals("setRegNumber crapat", "studentul2",s1.getRegNumber());
